b4dd764cc5d418cbdef55eafb67a662a0291b5a5
Alex Stein <astein.ny@gmail.com>
2018-12-31 15:34:00 -0600

    twoSum passing with better than 99.7% C++ solutions

--- leetcode/two_sum.cpp
+++ leetcode/two_sum.cpp
@@ -5,6 +5,8 @@
 #include<list>
 #include<unordered_map>
 
+#define SIZE 1000000
+
 using namespace std;
 
 class Solution {
@@ -57,11 +59,11 @@ public:
         return {-1, -1};
     }
 
-    /* twoSumOpt - by using a hashmap
-     * O(n) time-complexity
-     * O(n) extra-space
+    /* twoSumEvenBetter - by using a hashmap
+     * Theta(n) time-complexity
+     * Theta(n) extra-space
      */
-    vector<int> twoSumOpt(vector<int> &nums, int target) {
+    vector<int> twoSumEvenBetter(vector<int> &nums, int target) {
         /* create a hash-map */
         unordered_map<int,list<int>> idxs_by_value;
         for (int i = 0; i < nums.size(); i++) {
@@ -70,48 +72,82 @@ public:
         /* for each num, look in the hash map for its 'complement' */
         for (auto p : idxs_by_value) {
             int complement = target - p.first;
-            cout << "complement: " << complement << endl;
+            int sidx = p.second.front();
             auto cit = idxs_by_value.find(complement);
             /* does the complement value exist in the map */
-            if (cit == idxs_by_value.end())
-                break;
+            if (cit == idxs_by_value.end()) {
+                continue;
+            }
             auto cidxs = cit->second;
             /* is the complement value the same as the source value */
             if (cidxs.front() == p.first) {
                 if (cidxs.size() == 1) {
                     /* can't use the same element twice */
-                    break;
+                    continue;
                 }
                 /* return the index of the next satisfying element that's not the source */
-                return {p.first, cidxs.back()};
+                return {sidx, cidxs.back()};
             }
             /* the complement exists and isn't the same as the source value */
-            return {p.first, cidxs.front()};
+            return {sidx, cidxs.front()};
 
         }
         /* default return value */
         return {-1, -1};
     }
+
+    /* twoSumBest - hashmap in a single pass
+     * O(n) time-complexity
+     * O(n) extra-space
+     * 
+     * No need to check for "same element" as we dont insert into the map
+     * until after checking for the existence of the complement.
+     */
+    vector<int> twoSumBest(vector<int> &nums, int target) {
+        unordered_map<int,int> idx_by_value;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            auto c_pair = idx_by_value.find(complement);
+            if (c_pair != idx_by_value.end()) {
+                return {i, c_pair->second};
+            }
+            idx_by_value[nums[i]] = i;
+        }
+        return {-1, -1};
+    }
+    
 };
 
-int main() {
+int main(int argc, char* argv[]) {
+    /* which implementation to use */
+    string implementation = argv[1];
 
     /* inputs */
-    int arr[10000];
-    for (int i = 0; i < 10000; i++) {
+    int arr[SIZE+1];
+    for (int i = 0; i < SIZE; i++) {
         arr[i] = i + 1;
     }
-    int len = sizeof(arr) / sizeof(int);
-    int target = 19999;
+    arr[SIZE] = 1;
+    int len = SIZE+1;
+    int target = SIZE + SIZE - 1;
 
     /* solution */
     Solution S;
     vector<int> v = vector<int>(arr, arr + len); 
     random_shuffle(v.begin(), v.end());
-    //vector<int> result = S.twoSum(v, target);
-    //vector<int> result = S.twoSumBetter(v, target);
-    vector<int> result = S.twoSumOpt(v, target);
-    cout << ", " << target << endl;
+    vector<int> result;
+    if (implementation == "naive") {
+        result = S.twoSum(v, target);
+    }
+    if (implementation == "better") {
+        result = S.twoSumBetter(v, target);
+    }
+    if (implementation == "evenbetter") {
+        result = S.twoSumEvenBetter(v, target);
+    }
+    if (implementation == "best") {
+        result = S.twoSumBest(v, target);
+    }
     cout << "solution indices: ";
     for (int i = 0; i < result.size(); i++) {
         cout << result[i] << " ";
